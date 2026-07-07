d2f6a0a96e5d0426cd03ac32e2132284984b92dc
cateagle <cateagle@example.local>
2026-07-07 15:06:01 +0200

    Update 0001_initial_schema.sql

--- backend/src/migrations/0001_initial_schema.sql
+++ backend/src/migrations/0001_initial_schema.sql
@@ -10,16 +10,17 @@ CREATE TABLE IF NOT EXISTS field_documentation (
 CREATE TABLE IF NOT EXISTS users (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
     uuid        BLOB    NOT NULL UNIQUE CHECK(LENGTH(uuid) = 16),
-    created_at  INTEGER NOT NULL,
+    created_at  INTEGER NOT NULL, -- unix millis
     created_by  INTEGER NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
     username    TEXT    NOT NULL UNIQUE(LOWER(username)),
+    role        TEXT    NOT NULL CHECK(role IN ('annotator', 'scientist', 'admin'),
     expert_level INTEGER NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS labels (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
     uuid        BLOB    NOT NULL UNIQUE CHECK(LENGTH(uuid) = 16),
-    created_at  INTEGER NOT NULL,
+    created_at  INTEGER NOT NULL, -- unix millis
     created_by  INTEGER NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
     scope       TEXT    NOT NULL CHECK(LENGTH(scope) < 128),
     title       TEXT    NOT NULL CHECK(LENGTH(title) < 128),
@@ -30,7 +31,7 @@ CREATE TABLE IF NOT EXISTS labels (
 CREATE TABLE IF NOT EXISTS cameras (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
     uuid        BLOB    NOT NULL UNIQUE CHECK(LENGTH(uuid) = 16),
-    created_at  INTEGER NOT NULL,
+    created_at  INTEGER NOT NULL, -- unix millis
     created_by  INTEGER NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
     title       TEXT    NOT NULL UNIQUE CHECK(LENGTH(title) < 128),
     metadata    TEXT,
@@ -40,20 +41,20 @@ CREATE TABLE IF NOT EXISTS cameras (
 CREATE TABLE IF NOT EXISTS regions (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
     uuid        BLOB    NOT NULL UNIQUE CHECK(LENGTH(uuid) = 16),
-    created_at  INTEGER NOT NULL,
+    created_at  INTEGER NOT NULL, -- unix millis
     created_by  INTEGER NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
     title       TEXT    NOT NULL UNIQUE CHECK(LENGTH(title) < 128),
-    metadata    TEXT,
+    metadata    TEXT, -- json
     description TEXT    CHECK(description IS NULL OR LENGTH(description) < 1024)
 );
 
 CREATE TABLE IF NOT EXISTS dives (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
     uuid        BLOB    NOT NULL UNIQUE CHECK(LENGTH(uuid) = 16),
-    created_at  INTEGER NOT NULL,
+    created_at  INTEGER NOT NULL, -- unix millis
     created_by  INTEGER NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
     title       TEXT    NOT NULL UNIQUE CHECK(LENGTH(title) < 128),
-    metadata    TEXT,
+    metadata    TEXT, -- json
     description TEXT    CHECK(description IS NULL OR LENGTH(description) < 1024),
     region_id   INTEGER NOT NULL REFERENCES regions(id) ON DELETE RESTRICT,
     camera_id   INTEGER NOT NULL REFERENCES cameras(id) ON DELETE RESTRICT
@@ -62,27 +63,27 @@ CREATE TABLE IF NOT EXISTS dives (
 CREATE TABLE IF NOT EXISTS image_statuses (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
     uuid        BLOB    NOT NULL UNIQUE CHECK(LENGTH(uuid) = 16),
-    created_at  INTEGER NOT NULL,
+    created_at  INTEGER NOT NULL, -- unix millis
     created_by  INTEGER NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
     title       TEXT    NOT NULL UNIQUE CHECK(LENGTH(title) < 128),
-    metadata    TEXT,
+    metadata    TEXT, -- json
     description TEXT    CHECK(description IS NULL OR LENGTH(description) < 1024),
 );
 
 CREATE TABLE IF NOT EXISTS pair_statuses (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
     uuid        BLOB    NOT NULL UNIQUE CHECK(LENGTH(uuid) = 16),
-    created_at  INTEGER NOT NULL,
+    created_at  INTEGER NOT NULL, -- unix millis
     created_by  INTEGER NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
     title       TEXT    NOT NULL UNIQUE CHECK(LENGTH(title) < 128),
-    metadata    TEXT,
+    metadata    TEXT, -- json
     description TEXT    CHECK(description IS NULL OR LENGTH(description) < 1024),
 );
 
 CREATE TABLE IF NOT EXISTS images (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
     uuid        BLOB    NOT NULL UNIQUE CHECK(LENGTH(uuid) = 16),
-    created_at  INTEGER NOT NULL,
+    created_at  INTEGER NOT NULL, -- unix millis
     created_by  INTEGER NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
     filename    TEXT    NOT NULL CHECK(LENGTH(filename) < 256),
     filepath    TEXT    NOT NULL UNIQUE CHECK(LENGTH(filepath) < 1024),
@@ -90,15 +91,15 @@ CREATE TABLE IF NOT EXISTS images (
     status_id   INTEGER REFERENCES image_statuses(id) ON DELETE RESTRICT,
     size_x      INTEGER NOT NULL CHECK(size_x > 0),
     size_y      INTEGER NOT NULL CHECK(size_y > 0),
-    camera_pos  TEXT,
-    metadata    TEXT,
+    camera_pos  TEXT, -- json
+    metadata    TEXT, -- json
     difficulty  INTEGER,
     priority    INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS image_pairs (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    created_at  INTEGER NOT NULL,
+    created_at  INTEGER NOT NULL, -- unix millis
     created_by  INTEGER NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
     image1_id   INTEGER NOT NULL REFERENCES images(id) ON DELETE RESTRICT,
     image2_id   INTEGER NOT NULL REFERENCES images(id) ON DELETE RESTRICT,
@@ -112,7 +113,7 @@ CREATE TABLE IF NOT EXISTS image_pairs (
 CREATE TABLE IF NOT EXISTS point_annotations (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
     uuid        BLOB    NOT NULL UNIQUE CHECK(LENGTH(uuid) = 16),
-    created_at  INTEGER NOT NULL,
+    created_at  INTEGER NOT NULL, -- unix millis
     created_by  INTEGER NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
     pair_id     INTEGER NOT NULL REFERENCES image_pairs(id) ON DELETE RESTRICT,
     label_id    INTEGER REFERENCES labels(id) ON DELETE RESTRICT,
